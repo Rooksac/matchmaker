e55dc0e4712aae40358796bf5eae1e74983e5fea
Zachary Dean <Zakedoubled@gmail.com>
2022-09-13 15:40:27 -0400

    more matchmaker home changes

--- matchmaker-client/src/components/MatchmakerHome.js
+++ matchmaker-client/src/components/MatchmakerHome.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TileContainer from './TileContainer'
+import DaterTile from './DaterTile'
+import ClientTile from './ClientTile'
 
 export default function MatchmakerHome({matchmaker}) {
+  const [showDaters, setShowDaters] = useState(false)
+  const [availableClients, setAvailableClients] = useState([])
+  function handleClick(){
+    setShowDaters(!showDaters)
+    fetch('http://localhost:9292/availabledaters')
+    .then((res)=>res.json())
+    .then((data)=>setAvailableClients(data))
+  }
   return (
     <div>
 
       <h1>{`Welcome back ${matchmaker[0].username}!`}</h1>
-      
+      {matchmaker[1].length<1?<button onClick = {handleClick}>Search for a client</button>:<ClientTile dater = {matchmaker[1][0]}/>}
+      {matchmaker[1].length<2?<button onClick = {handleClick}>Search for a client</button>:<ClientTile dater = {matchmaker[1][1]}/>}
+      {matchmaker[1].length<3?<button onClick = {handleClick}>Search for a client</button>:<ClientTile dater = {matchmaker[1][2]}/>}
 
-      <TileContainer clients = {matchmaker[1]} />
+      {showDaters?<TileContainer tiles = {availableClients} />:null}
 
     </div>
   )
